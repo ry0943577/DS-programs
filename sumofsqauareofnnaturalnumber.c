38085f173faf336554252b83bb6fd1cc6c7eddc9
ry0943577 <ry0943577@gmail.com>
2025-09-02 10:50:55 +0530

    Add files via upload

---
+++ sumofsqauareofnnaturalnumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int b;
+	int sum =0;
+	 printf("Enter a number: ");
+	 scanf("%d",&b);
+	 
+	 for(int i=0;i<=b;i++){
+	 	sum = sum + i*i;
+	 	printf("The sum of square of %d natural number is: %d \n",i,sum);
+	 	
+	 }
+}
