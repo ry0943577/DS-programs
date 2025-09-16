72e1d2561dfe3f9034f3f12a056e500327f4cba2
ry0943577 <ry0943577@gmail.com>
2025-09-16 11:01:00 +0530

    Add files via upload

---
+++ callbyreference.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void swap(int *x, int *y){
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+	printf("Inside the Swap X and Y: %d and %d\n",*x,*y);
+}
+
+int main(){
+	int c;
+	int d;
+	printf("value of C: ");
+	scanf("%d",&c);
+	printf("value of D: ");
+	scanf("%d",&d);
+	printf("Value of C and D before swapping: %d and %d\n",c,d);
+	swap(&c,&d);
+	printf("Value of C and D after swapping: %d and %d",c,d);
+	return 0;
+}
+
+
